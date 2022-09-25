b1b7ce21c4574122a89dc87e756cb2f2ceee6664
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-09-25 21:54:09 +0100

    Add validations to Author model

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :author do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { "Jane" }
+    last_name { "Dolph" }
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(described_class.new(first_name: 'Thomas', last_name: 'Smith')).to be_valid
+  end
+
+  it 'is not valid without first name' do
+    expect(described_class.new(last_name: 'Smith')).not_to be_valid
+  end
+
+  it 'is not valid without last name' do
+    expect(described_class.new(first_name: 'Thomas')).not_to be_valid
+  end
 end
