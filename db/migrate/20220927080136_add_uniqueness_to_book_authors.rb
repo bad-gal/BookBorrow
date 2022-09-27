261f9e1885e4794df68ab177aa943d2b7c87c738
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-09-27 19:41:32 +0100

    Rollback uniqueness on BookAuthors table

--- db/migrate/20220927080136_add_uniqueness_to_book_authors.rb
+++
@@ -1,6 +0,0 @@
-class AddUniquenessToBookAuthors < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :book_authors, :author_id
-    add_index :book_authors, :author_id, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_080136) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_153713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_080136) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_book_authors_on_author_id", unique: true
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
     t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
