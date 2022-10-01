d3b58b1f9e052acb4cbf8bf378914a2ab230e6c7
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-10-01 20:12:09 +0100

    Add featured and available books to the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.available
+    @featured = Book.featured
   end
 end
