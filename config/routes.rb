6ed171820747b748e076fc13a77e34c60124b96c
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-10-08 23:20:19 +0100

    Include first name, last name and location in Sign up form. Add sign out link in nav bar. Ensure that user returns to the home page when logging out of application

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    # Redirects signing out users back to sign-in
-    get "users", to: "devise/sessions#new"
-  end
-
-  devise_for :users
 
   # Defines the root path route ("/")
   root "home#index"
 
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "home#index"
+  end
+
+  devise_for :users
   resources :books
 end
