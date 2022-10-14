46f5e4ef8f3eb0a13026bf689da5ab70c524f8f2
Andrien Ricketts <knowledgehut.developments@gmail.com>
2022-10-15 00:05:06 +0100

    Add helper full_name  method for author and update styling on author partial form

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module AuthorsHelper
+  # full_name(author.first_name, author.last_name)
   def full_name(first_name, last_name)
     "#{first_name} #{last_name}"
   end
